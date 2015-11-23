5558b21834bb058eb8a158ad6a25b51cdb2f7df7
mfinzi <mfinzi@hmc.edu>
2015-11-22 16:09:41 -0800

    Update finalProject.cpp

--- finalProject.cpp
+++ finalProject.cpp
@@ -1,10 +1,14 @@
 #include "EasyPIO.h"
 #include <stdio.h>
+#include <math.h>
+#define PI 3.14159265359
 
 #define LOAD_PIN 26
 #define DONE_PIN 19
 #define SOUND_PIN 4
 #define EXIT_PIN 18
+
+
 
 // Masks for our strings.
 typedef enum{
@@ -94,6 +98,59 @@ void runHarp() {
 }
 // TODO: Make the note sound better + ADC SPI communication
 //                                    + amp -> speaker????
+class Harp
+{
+private:
+  float _noteWeights[8];
+  void writeNotes(int time);
+  void playNotes(int dur, int startT);
+  void updateWeights();
+public:
+  Harp(){for(int i=0; i<8; ++i) _noteWeights[i]=0; };
+  void runHarp();
+                               // c   d   e   f   g   a   b   c
+  static const int NOTEFREQ[] = {262,294,330,349,392,440,494,523};
+  static const float PWMFREQUENCY = 500000;
+  static const int DT = 100;//100us between dac updates for audio
+
+};
+
+// Plays combination of notes according to _noteWeights, takes in
+// time in us since start
+void Harp::writeNoteValue(int t){
+  float summedDuty = 0;
+  for (int i=0; i<8; ++i){            /// 1mil factor conversion from us->s
+    summedDuty += _noteWeights[i]*cos(2*pi*NOTEFREQ[i]*t/1000000); 
+  }
+  setPWM(PWMFREQUENCY,summedDuty);
+}
+
+// Function takes in a duration to play set weights in (us) and a startTime (us)
+void Harp::playNotes(int duration, int startTime){
+  for (int nowTime = startTime; nowTime < startTime + duration; nowTime += DT){
+    writeNoteValue(nowTime);
+  }
+}
+// Function executes SPI transaction with FPGA to update the note weights
+// Should also normalize
+void Harp::updateWeights(){
+  char sendAction = 0x01; // doesn't really matter yet what we send.
+  for (int i=0; i<8; ++i){
+    printf("for string %d we get %c",i,spiSendReceive(sendAction));
+    // TODO test and continue from here
+    // _noteWeights[i] = float(spiSendReceive(sendAction))/128.;
+    // then normalize
+  }
+}
+// Function for continous operation of the harp playing music. Add exit condition.
+void Harp::srunHarp(){}
+
+
+
+
+
+
+
 
 void testScale() {
   char array[] =  {'c', 'd', 'e' ,'f', 'g', 'a', 'b', 'C'};
@@ -110,10 +167,17 @@ void testScale() {
 // STRETCH: Playback and play mode.
 int main() {
   pioInit();
+  pwmInit();
   spiInit(244000, 0);
   pinMode(LOAD_PIN, OUTPUT);
   pinMode(DONE_PIN, INPUT);
   pinMode(SOUND_PIN, OUTPUT);
   pinMode(EXIT_PIN, INPUT);
-  testScale(); 
+  //testScale(); 
+  Harp myHarp();
+  while(1){
+	myHarp.updateWeights();
+	delayMicros(100);
+  }
+  
 }
