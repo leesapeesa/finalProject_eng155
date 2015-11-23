a00c16807fcde4644965e11d9acf88051e639137
leesapeesa <lyin@g.hmc.edu>
2015-11-22 20:43:20 -0800

    added test for multi note output

--- finalProject.cpp
+++ finalProject.cpp
@@ -5,8 +5,8 @@
 
 #define LOAD_PIN 26
 #define DONE_PIN 19
-#define SOUND_PIN 4
-#define EXIT_PIN 18
+#define SOUND_PIN 18
+#define EXIT_PIN 15
 
 class Harp
 {
@@ -16,8 +16,8 @@ private:
   void playNotes(int dur, int startT);
   
 public:
-  Harp(){for(int i=0; i<8; ++i) _noteWeights[i]=0; };
-  void srunHarp();
+  Harp();
+  void runHarp();
   void updateWeights();
   static const int NOTEFREQ[8]; 
   static const float PWMFREQUENCY = 500000;
@@ -27,6 +27,13 @@ public:
 
                   // c   d   e   f   g   a   b   c
 const int Harp::NOTEFREQ[] = {262,294,330,349,392,440,494,523};
+
+Harp::Harp(){
+	for(int i=0; i<8; ++i) _noteWeights[i] = 0; 
+	pinMode(SOUND_PIN, OUTPUT);
+	pinMode(EXIT_PIN, INPUT);
+}
+
 // Plays combination of notes according to _noteWeights, takes in
 // time in us since start
 void Harp::writeNoteValue(int t){
@@ -48,21 +55,18 @@ void Harp::playNotes(int duration, int startTime){
 void Harp::updateWeights(){
   char sendAction = 0x01; // doesn't really matter yet what we send.
   for (int i=0; i<8; ++i){
-    printf("for string %d we get %c",i,spiSendReceive(sendAction));
-    // TODO test and continue from here
-    // _noteWeights[i] = float(spiSendReceive(sendAction))/128.;
+    _noteWeights[i] = float(spiSendReceive(sendAction))/128.;
     // then normalize
   }
 }
 // Function for continous operation of the harp playing music. Add exit condition.
-void Harp::srunHarp(){}
+void Harp::runHarp(){}
 
 void Harp::testScale() {
-  for(unsigned int i = 0; i < numNotes; ++i) {
-    for (unsigned int j = 0; j < numNotes; ++j) {
-      
-    } 
-  }
+  _noteWeights[] = {0,.33,0,0,.33,0,0,.33};
+  playNotes(1000000000,0);
+  _noteWeights[] = {0,0,0,0,0,.5,0,.5};
+  playNotes(1000000000,0);
 }
 // STRETCH: Send notes + duration to FPGA
 // STRETCH: Keep track of length of note.
@@ -71,17 +75,9 @@ int main() {
   pioInit();
   pwmInit();
   spiInit(244000, 0);
-  pinMode(LOAD_PIN, OUTPUT);
-  pinMode(DONE_PIN, INPUT);
-  pinMode(SOUND_PIN, OUTPUT);
-  pinMode(EXIT_PIN, INPUT);
   Harp myHarp;
   
   myHarp.testScale();
-  
-  while(1){
-	myHarp.updateWeights();
-	delayMicros(1000000);
-  }
+ 
   
 }
