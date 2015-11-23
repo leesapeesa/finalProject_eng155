258f23e4524e7d1deca7b843ff189b04acd26c75
Lisa Yin <lyin@g.hmc.edu>
2015-11-22 20:55:09 -0800

    changing makefile

--- finalProject.cpp
+++ finalProject.cpp
@@ -22,14 +22,14 @@ public:
   void updateWeights();
   void testScale();
   static const int NOTEFREQ[8]; 
-  static const float PWMFREQUENCY = 500000;
+  static const float PWMFREQUENCY;
   static const int DT = 100;//100us between dac updates for audio
   static const unsigned int numNotes = 8;
 };
 
                   // c   d   e   f   g   a   b   c
 const int Harp::NOTEFREQ[] = {262,294,330,349,392,440,494,523};
-
+const float Harp::PWMFREQUENCY = 500000;
 Harp::Harp(){
 	for(int i=0; i<8; ++i) _noteWeights[i] = 0; 
 	pinMode(SOUND_PIN, OUTPUT);
